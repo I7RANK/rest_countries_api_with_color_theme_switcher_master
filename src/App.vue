28dbdea724e43eb882a50d0e57d4548f397abebb
I7RANK <frafgh2000@hotmail.com>
2022-05-09 20:38:07 -0500

    First commit - filters workig with mocked data

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="flex justify-between py-4 px-2 shadow-sm">
+    <h1 class="text-sm font-bold">Where in the world?</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+header {
+  background: var(--secondary-background);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,115 @@
+<script>
+import magnifyingGlassSolid from '../assets/icons/magnifying-glass-solid.svg';
+import allCountries from './countries';
+
+let timerStopTyping;
+
+function removeDiacritics(text) {
+  return text.normalize('NFD').replace(/[\u0300-\u036f]/g,"");
+}
+
+export default {
+  data() {
+    return {
+      magnifyingGlassSolid,
+      filterByName: 'all',
+      filterByRegion: 'all',
+      regions: ['all', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'],
+      countries: [],
+      countriesFiltered: [],
+    };
+  },
+  watch: {
+    filterByRegion(newValue) {
+      if (newValue === 'all') {
+        this.getAllCountries();
+        return;
+      }
+      this.getCountriesByRegion(this.filterByRegion);
+    },
+    filterByName(newValue) {
+      console.log(newValue);
+      this.countriesFiltered = this.countries.filter(country => {
+        let name = removeDiacritics(country.name.common).toLowerCase();
+
+        if (name.includes(newValue.toLowerCase())) return country;
+      });
+    },
+    countries(newValue) {
+      this.$refs.inputFilterByName.value = '';
+      this.countriesFiltered = newValue;
+    }
+  },
+  methods: {
+    async getAllCountries() {
+      const url = 'https://restcountries.com/v3.1/all';
+
+      try {
+        const res = await fetch(url);
+        this.countries = (await res.json());
+      } catch (error) {
+        console.log('Error! Could not reach the API. ' + error);
+      }
+    },
+    async getCountriesByRegion(region) {
+      const url = `https://restcountries.com/v3.1/region/${region}`;
+
+      try {
+        const res = await fetch(url);
+        this.countries = (await res.json());
+      } catch (error) {
+        console.log('Error! Could not reach the API. ' + error);
+      }
+    },
+    applyFilter(e) {
+      this.filterByRegion = e.target.value;
+    },
+    isTyping(e) {
+      clearTimeout(timerStopTyping);
+
+      timerStopTyping = setTimeout(() => {
+        this.filterByName = e.target.value;
+      }, 1000);
+    },
+  },
+  mounted() {
+    // this.getAllCountries();
+    this.countries = allCountries;
+  }
+};
+</script>
+
+<template>
+  <div>
+    <section class="filter-section flex flex-wrap p-4">
+      <div class="search-content min-w-full mb-4 flex items-center justify-around p-3 rounded-md shadow-md">
+        <img :src="magnifyingGlassSolid" alt="" class="h-4 mx-3">
+        <input ref="inputFilterByName" @keyup="isTyping" class="grow p-2" type="text" placeholder="Search for a country...">
+      </div>
+      <div class="filter-regions-content p-3 rounded-md shadow-md">
+        <select name="regions" id="regions" @input="this.applyFilter">
+          <option :key="region" :value="region" v-for="region in regions">{{ region }}</option>
+        </select>
+      </div>
+    </section>
+
+    <section class="countries-content">
+      {{ filterByRegion }} cant: {{ countriesFiltered.length }}
+      <ul>
+        <li :key="country" v-for="country in countriesFiltered">{{ country.name.common }}</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<style>
+.search-content, .filter-regions-content, .filter-regions-content select {
+  background: var(--secondary-background);
+}
+/* main {
+  background: #000;
+}
+img {
+  filter: invert(90%);
+} */
+</style>
